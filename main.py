49fc4da95713f1bcfda10a4330a4a49bb39924dc
SiddharthChakraborty1 <sc9921777@gmail.com>
2023-05-29 18:38:22 +0000

    removed testing code and finalized the welcome message

--- main.py
+++ main.py
@@ -9,14 +9,6 @@ bot = telebot.TeleBot(TOKEN)
 WELCOME_MESSAGE = "Hello there! I am a bot made by Siddharth.. \nSend me the command /pic and I will send you NASA's pic of the day"
 
 
-@bot.message_handler(func=lambda mesg: True)
-def check_bharti(message):
-  if message.text == "bharti-t71402":
-    bot.send_message(
-      message.chat.id,
-      "Heyyy Bhartiiiiii! How're you doing! my owner loves railing youuuuuuu")
-
-
 @bot.message_handler(commands=['start', 'hello'])
 def send_welcome(message):
   bot.reply_to(message, WELCOME_MESSAGE)
